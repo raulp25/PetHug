c85bdd8659d669f4415bf1995cbd86f95e71f2b6
Raul <103614244+raulp25@users.noreply.github.com>
2023-10-21 13:36:24 -0600

    Fix changed NameValidator isToLong limit to 12 characters again

--- pethug/Core/Utilities/Validation/Validatable.swift
+++ pethug/Core/Utilities/Validation/Validatable.swift
@@ -148,7 +148,7 @@ struct NameValidator: Validatable {
         Publishers.CombineLatest4(
             isEmpty(with: publisher),
             isToShort(with: publisher, count: 2),
-            isToLong(with: publisher, count: 12),
+            isToLong(with: publisher, count: 13),
             hasSpecialChars(with: publisher)
         ).removeDuplicates(by: { prev, curr in
             prev == curr
